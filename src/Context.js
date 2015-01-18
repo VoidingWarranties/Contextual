aab9cb5ed679da140255fac30b05f7abf7967d0b
Jordan Horwich <jordan@voidingwarranties.com>
2015-01-17 21:48:12 -0500

    Implement basic context manipulation.
    This one's for Courtney...

--- src/Context.js
+++ src/Context.js
@@ -1,1 +1,15 @@
-alert("Context.js");
+// Returns the contexts (short description with bolded matched words) of the
+// search results. This is only a function to allow the implementation to be
+// changed if Google ever changes the format of their HTML.
+function getSearchContexts() {
+  return document.querySelectorAll(".st");
+}
+
+function insertLinksIntoContext(context) {
+  context.innerHTML = "who dat who dat?"; // Courtney you know what to do: http://xkcd.com/1288/
+}
+
+var contexts = getSearchContexts();
+for (var i = 0; i < contexts.length; ++i) {
+  contexts[i] = insertLinksIntoContext(contexts[i]);
+}
