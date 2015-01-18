c9ce0c50e442b8641ddd71d46a4701e399f0b1b8
Jordan Horwich <jordan@voidingwarranties.com>
2015-01-17 21:02:37 -0500

    Implement programmatic injection of Context.js
    Context.js should only be injected into Google search results, but
    currently it is being injected into all Google pages.
    
    Context.js is just dummy code in this commit.

--- src/EventPage.js
+++ src/EventPage.js
@@ -1,1 +1,19 @@
-alert("hi");
+chrome.tabs.onUpdated.addListener(function(id, info, tab) {
+  if (info.status !== "complete") {
+    return;
+  }
+
+  // Parse the tab's URL.
+  // The hostname is contained in parser.hostname
+  // Credit goes to: https://gist.github.com/jlong/2428561
+  var parser = document.createElement('a');
+  parser.href = tab.url;
+
+  if (parser.hostname !== "www.google.com") {
+    return;
+  }
+
+  // Execute Context.js in the context of the current page
+  // (which should be a Google search result).
+  chrome.tabs.executeScript(null, {file: "Context.js"});
+});
